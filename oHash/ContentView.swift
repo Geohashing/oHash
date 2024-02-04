21feb85bf42e0854723c4cec4979933399a75ea2
Brendan T White <brendan@thespia.com>
2024-02-04 12:15:15 +1030

    New iOS app, with tests, no storage

---
+++ oHash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oHash
+//
+//  Created by Brendan White on 4/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
