ebca64c1032d784bc136d2c6e44aadcf70401863
Brendan T White <brendan@thespia.com>
2024-02-04 13:17:18 +1030

    Initial, super-basic mockup. Nothing works yet - it's just a rough outline of how it will end up looking.

--- oHash/ContentView.swift
+++ oHash/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            HStack{
+                
+                Button(
+                    action: {print("clicked gearshape icon") },
+                    label: {Image(systemName: "gearshape")}
+                )
+                Spacer()
+                
+                Button(
+                    action: {print("clicked arrowshape.left icon") },
+                    label: {Image(systemName: "arrowshape.left")}
+                )
+
+                Text("Today")
+                
+                Button(
+                    action: {print("clicked arrowshape.right icon") },
+                    label: {Image(systemName: "arrowshape.right")}
+                )
+
+                Spacer()
+                Button(
+                    action: {print("clicked calendar icon") },
+                    label: {Image(systemName: "calendar")}
+                )
+
+            }
+            .padding([.leading,.trailing], 20)
+
+            Map()
+
+            Text("Graticule Info Section")
+                .padding(50)
+            
+
         }
-        .padding()
     }
 }
 
