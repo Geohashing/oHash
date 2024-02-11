04c6f96facb3ed95214dcb4288aac618f2f8ad73
Brendan T White <brendan@thespia.com>
2024-02-11 18:30:23 +1030

    Using toolbar nicely now

--- oHash/ContentView.swift
+++ oHash/ContentView.swift
@@ -10,42 +10,98 @@ import MapKit
 
 struct ContentView: View {
     
-    @State private var blah = "blah blah blah"
+    @State private var date = Date.now
+    @State private var blah = "no action yet"
     
     var body: some View {
         NavigationStack {
             
             VStack {
-                
                 Map()
-                
-                Text(blah)
-                    .padding(50)
-                
+                VStack{
+                    Text(blah)
+                    Text(date, style: .date)
+                }.padding(50)
             }
+            
             .navigationBarTitleDisplayMode(.inline)
-            .toolbarRole(.browser)
             .toolbar {
                 
                 ToolbarItem(placement: .topBarLeading) {
-                    Button(
-                        action: {blah = "clicked gearshape icon" },
-                        label: {Image(systemName: "gearshape")}
+                    
+                    Menu(content: {
+                        
+                        Button(
+                            action: {blah = "clicked 1.circle" },
+                            label: {
+                                Image(systemName: "1.circle")
+                                Text("One")
+                            }
+                        )
+                        
+                        Button(
+                            action: {blah = "clicked 2.circle" },
+                            label: {
+                                Image(systemName: "2.circle")
+                                Text("Two")
+                            }
+                        )
+                        
+                        Button(
+                            action: {blah = "clicked 3.circle" },
+                            label: {
+                                Image(systemName: "3.circle")
+                                Text("Three")
+                            }
+                        )
+                        
+                    },
+                         label: {Image(systemName: "gearshape")}
                     )
+                    
                 }
                 
                 ToolbarItem(placement: .principal) {
-                    Button("today") {
-                        print("today tapped!")
-                        blah = "clicked date"
-                    }
+                    
+                    DatePicker(
+                        "What date do you want to check?",
+                        selection: $date,
+                        displayedComponents: .date
+                    ).labelsHidden()
+                    
                 }
-                
+
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button(
-                        action: {blah = "clicked bell icon" },
-                        label: {Image(systemName: "bell")}
+                    
+                    Menu(content: {
+                        
+                        Button(
+                            action: {blah = "clicked 1.square" },
+                            label: {
+                                Image(systemName: "1.square")
+                                Text("One")
+                            }
+                        )
+                        
+                        Button(
+                            action: {blah = "clicked 2.square" },
+                            label: {
+                                Image(systemName: "2.square")
+                                Text("Two")
+                            }
+                        )
+                        
+                        Button(
+                            action: {blah = "clicked 3.square" },
+                            label: {
+                                Image(systemName: "3.square")
+                                Text("Three")
+                            }
+                        )
+                        
+                    }, label: {Image(systemName: "bell")}
                     )
+
                     
                 }
                 
@@ -54,6 +110,7 @@ struct ContentView: View {
                 
                 
             }
+            
         }
     }
         
