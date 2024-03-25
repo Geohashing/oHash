0d2d0d40c2073b5fc8b7e7e33e152ae53b6783c1
Brendan T White <brendan@thespia.com>
2024-03-25 21:09:23 +1030

    OK. We are now past most of the housekeeping, and we're up to the bit where we try to actually download the DJIA open.

--- oHash/Models/DowJonesDate.swift
+++ oHash/Models/DowJonesDate.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 enum DowJonesError {
+    case before_djia_existed_error
     case timeout_error
     case network_error
     case unknown_error
@@ -17,7 +18,6 @@ enum DowJonesError {
 enum DowJonesStatus {
     case already_had_it
     case too_early
-    case already_checked_recently
     case getting_it_now
     case just_got_it
     case error(_ error:DowJonesError)
@@ -25,19 +25,22 @@ enum DowJonesStatus {
 
 class DowJonesDate {
     private let date:Date
-
+    
     private let dowJonesStatus:DowJonesStatus
-
-    private let lastCheckTime = Date.now
-    private let lastCheckIntervalSeconds = 10
     
     private var dowJonesOpen:String?
-                
+    
     init(date: Date) {
         self.date = date
         
-        // First, get the DJ Open from the cache, if we can
-        dowJonesOpen = Self.getDowJonesOpenFor(date)
+        // First, reject with an error if date < first_djia_date
+        if (date < Self.first_djia_date) {
+            dowJonesStatus = .error(.before_djia_existed_error)
+            return
+        }
+        
+        // Next, get the DJ Open from the cache, if it's there
+        dowJonesOpen = Self.getFromCacheFor(date)
         if (dowJonesOpen != nil) {
             dowJonesStatus = .already_had_it
             return
@@ -45,35 +48,62 @@ class DowJonesDate {
         
         // Next, if it's before the DJ opening time for that date,
         // return status .too_early
+        if (date < Self.dowJonesOpenTimeFor(date)) {
+            dowJonesStatus = .too_early
+            return
+        }
         
-        
-        // if we get to here, the DJ Open should be available,
-        // but we don't have it yet.
-        // Is it too soon after the last check?
-        // If so, return status .already_checked_recently
-        
-        
-        // If we get here, the DJ Open should be available, and we
-        // haven't checked recently. So let's try to get it!!!
+        // If we get here, the DJ Open should be available.
+        // So let's try to get it!!!
         
         
         
         // if we get this far, we had a weird and unknown problem
-        dowJonesOpen = nil
         dowJonesStatus = .error(.unknown_error)
         return
     }
     
-    static func getDowJonesOpenFor(_ date:Date) -> String? {
+    static func dowJonesOpenTimeFor(_ date:Date) -> Date {
+        
+        let components = Calendar.current.dateComponents(
+            [.day, .month, .year], from: date)
+        
+        return Calendar.current.date(
+            from:DateComponents(
+                timeZone: TimeZone(abbreviation: "EST"),
+                year:   components.year,
+                month:  components.month,
+                day:    components.day,
+                hour:   09,
+                minute: 30,
+                second: 15 // we start checking 15 seconds after 9.30.
+                // See https://geohashing.site/geohashing/User_talk:Crox#How_soon_should_I_check_geo.crox.net.3F for details.
+            )
+        )!
+    }
+    
+    static func getFromCacheFor(_ date:Date) -> String? {
         
         let dateFormatter = ISO8601DateFormatter()
         dateFormatter.formatOptions = .withFullDate
         let dateString = dateFormatter.string(from: date)
-
+        
         let defaults = UserDefaults.standard
         let openString = defaults.object(forKey:dateString) as? String
         
         return openString
     }
-
+    
+    // the first ever DJIA open price was on 1928-10-01
+    static let first_djia_date = Calendar.current.date(
+        from:DateComponents(
+            timeZone: TimeZone(abbreviation: "EST"),
+            year: 1928,
+            month:  10,
+            day:    01,
+            hour:   00,
+            minute: 00
+        )
+    )!
+    
 }
