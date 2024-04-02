4b986975db8643c39f44533984c22df395739849
Brendan T White <brendan@thespia.com>
2024-04-02 21:52:40 +1030

    stubs

---
+++ oHash/Models/Graticule.swift
@@ -0,0 +1,12 @@
+//
+//  Graticule.swift
+//  oHash
+//
+//  Created by Brendan White on 2/4/2024.
+//
+
+import Foundation
+
+struct Graticule {
+    // TODO: Just a stub for now
+}
---
+++ oHash/Models/HashDate.swift
@@ -0,0 +1,12 @@
+//
+//  HashDate.swift
+//  oHash
+//
+//  Created by Brendan White on 2/4/2024.
+//
+
+import Foundation
+
+struct HashDate {
+    // TODO: Just a stub for now
+}
