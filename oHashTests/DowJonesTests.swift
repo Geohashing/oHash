8497183e30c8746f33f3567123942d5c8903ca97
Brendan T White <brendan@thespia.com>
2024-04-03 19:26:03 +1030

    Boilerplate

--- oHashTests/DowJonesTests.swift
+++ oHashTests/DowJonesTests.swift
@@ -1,5 +1,5 @@
 //
-//  oHashTests.swift
+//  DowJonesTests.swift
 //  oHashTests
 //
 //  Created by Brendan White on 4/2/2024.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import oHash
 
-final class oHashTests: XCTestCase {
+final class DowJonesTests: XCTestCase {
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -76,21 +76,5 @@ final class oHashTests: XCTestCase {
 
         
     }
-    
-    
-    //    func testExample() throws {
-    //        // This is an example of a functional test case.
-    //        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    //        // Any test you write for XCTest can be annotated as throws and async.
-    //        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-    //        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    //    }
-    
-    //    func testPerformanceExample() throws {
-    //        // This is an example of a performance test case.
-    //        self.measure {
-    //            // Put the code you want to measure the time of here.
-    //        }
-    //    }
     
 }
---
+++ oHashTests/GraticuleTests.swift
@@ -0,0 +1,28 @@
+//
+//  GraticuleTests.swift
+//  oHashTests
+//
+//  Created by Brendan White on 3/4/2024.
+//
+
+import XCTest
+
+final class GraticuleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCreate() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+}
