0d97d7eebfa6c6367966799125dc56f82352ce11
Brendan T White <brendan@thespia.com>
2024-04-14 12:58:09 +0930

    added a third logging category

--- oHash/Toolbox/Logger.swift
+++ oHash/Toolbox/Logger.swift
@@ -12,4 +12,5 @@ extension Logger {
     private static let appIdentifier = Bundle.main.bundleIdentifier ?? ""
     static let djopen    = Logger(subsystem: appIdentifier, category: "djopen"    )
     static let graticule = Logger(subsystem: appIdentifier, category: "graticule" )
+    static let misc      = Logger(subsystem: appIdentifier, category: "misc" )
 }
