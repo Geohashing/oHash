b36ab90d8d04913ace8e21d126110e938a3b3732
Brendan T White <brendan@thespia.com>
2024-04-14 19:19:40 +0930

    ... now with the *correct* boilerplate ...

--- oHash/Views/HashPoint.swift
+++ oHash/Views/HashPoint.swift
@@ -5,4 +5,14 @@
 //  Created by Brendan White on 14/4/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct HashPoint: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HashPoint()
+}
