472d3c54062eaea54f91e0529a40e3f8e33c4396
Brendan T White <brendan@thespia.com>
2024-04-14 12:57:08 +0930

    Created two new boilerplate views, and moved existing view to the Views group

---
+++ oHash/Views/InfoGrid.swift
@@ -0,0 +1,18 @@
+//
+//  InfoGrid.swift
+//  oHash
+//
+//  Created by Brendan White on 14/4/2024.
+//
+
+import SwiftUI
+
+struct InfoGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoGrid()
+}
---
+++ oHash/Views/OHashMap.swift
@@ -0,0 +1,18 @@
+//
+//  OHashMap.swift
+//  oHash
+//
+//  Created by Brendan White on 14/4/2024.
+//
+
+import SwiftUI
+
+struct OHashMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OHashMap()
+}
