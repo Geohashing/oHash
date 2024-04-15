6c58d83e0033b5e1fc842459c20de1b1b3951ebd
Brendan T White <brendan@thespia.com>
2024-04-15 23:09:19 +0930

    NOT working yet. Really just stubs / mocks.

--- oHash/Models/HashPoint.swift
+++ oHash/Models/HashPoint.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
+
+struct HashPoint: Identifiable {
+    let id = UUID()  // not convinced this is the best id, but it'll do for now...
+    
+    public let date:Date
+    public let graticule:Graticule
+    
+    public var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: graticule.bottomedge() + 0.5, longitude: graticule.leftedge() + 0.5)
+    }
+
+    @EnvironmentObject var state:OHashState
+
+    public static func forRegion(_ region:MKCoordinateRegion) -> [HashPoint] {
+        return [ 
+
+            HashPoint( date:Date.now, graticule: Graticule(coords: region.center) ),
+
+            HashPoint( date:Date.now, graticule: Graticule(coords: region.center).nextGratEast()  ),
+            HashPoint( date:Date.now, graticule: Graticule(coords: region.center).nextGratWest()  ),
+            HashPoint( date:Date.now, graticule: Graticule(coords: region.center).nextGratNorth() ),
+            HashPoint( date:Date.now, graticule: Graticule(coords: region.center).nextGratSouth() ),
+
+        ]
+    }
+    
+    
+}
